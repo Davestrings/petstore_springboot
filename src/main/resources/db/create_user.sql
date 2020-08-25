5998677422cc0129130330bb4cf20bdf4706172e
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-08-25 11:05:37 +0100

    pet store project

---
+++ src/main/resources/db/create_user.sql
@@ -0,0 +1,9 @@
+DROP USER IF EXISTS 'petuser'@'localhost';
+CREATE USER 'petuser'@'localhost' IDENTIFIED BY 'petuser123';
+GRANT ALL PRIVILEGES ON petstore_db.* TO 'petuser'@'localhost';
+FLUSH PRIVILEGES ;
+
+
+DROP DATABASE IF EXISTS petstore_db;
+CREATE DATABASE petstore_db;
+
